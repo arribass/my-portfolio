fb33e233ba7734bdc723d2b44c7fe1992f7c82ba
arribass <55694226+arribass@users.noreply.github.com>
2023-06-12 18:12:24 +0200

    vuelta workflows

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Prueba para quitar mi workflow</p>
+        <p>A ver si se lanza el workflow</p>
         <a
           className="App-link"
           href="https://reactjs.org"
